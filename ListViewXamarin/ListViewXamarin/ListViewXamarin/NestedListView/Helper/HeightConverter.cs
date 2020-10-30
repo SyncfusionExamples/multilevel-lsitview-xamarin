a9dcf4b905f3928651f0dad23969db1b1cc6cb35
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-10-30 12:58:09 +0530

    XAMARIN-37739 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/NestedListView/Helper/HeightConverter.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public class HeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var listView = parameter as SfListView;
+            var bindingContext = (listView.BindingContext as DetailsContactInfo);
+
+            if (bindingContext == null) return 0;
+
+            var items = bindingContext.Members;
+            return listView.ItemSize * items.Count;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
